2a2e198817d6ca2ba511a430cf0a4541e9eeb6de
luyosamson <luyosamson@gmail.com>
2022-09-24 14:00:41 +0300

    Assignment completed

--- src/DogPics.js
+++ src/DogPics.js
@@ -13,17 +13,46 @@ function DogPics() {
         console.log("setState");
         setImages(data.message);
       });
-  });
+  },[]);
 
   console.log("render");
 
   return (
     <div>
       {images.map((image) => (
-        <img src={image} key={image} />
+        <img src={image} key={image} alt="dog" />
       ))}
     </div>
   );
 }
 
 export default DogPics;
+
+// function App() {
+//   const [count, setCount] = useState(0);
+//   const [text, setText] = useState("");
+
+//   useEffect(() => {
+//     document.title = text;
+//   }, [text]);
+
+//   useEffect(() => {
+//     setTimeout(() => setCount(0), 5000);
+//   }, []);
+
+//   console.log("Component rendering");
+
+//   return (
+//     <div>
+//       <button onClick={() => setCount((count) => count + 1)}>
+//         I've been clicked {count} times
+//       </button>
+//       <input
+//         type="text"
+//         placeholder="Type away..."
+//         value={text}
+//         onChange={(e) => setText(e.target.value)}
+//       />
+//     </div>
+//   );
+// }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+// import App from "./App";
+import DogPics from "./DogPics"
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<DogPics />
+
+, document.getElementById("root"));
